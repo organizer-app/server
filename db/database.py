35bcb9d092b47e0f4719d1b6dd8de95d1864121f
Jeffrey Goldsmith <phoenixofstone@yahoo.ca>
2018-03-02 15:10:11 -0500

    Added migrations

--- db/database.py
+++ db/database.py
@@ -1,2 +1,10 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 database = SQLAlchemy()
+
+engine = create_engine('postgresql://localhost/organizer_development', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
