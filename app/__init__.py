0cd13d6a70f6ec4b2d29f05a20c47d98719e63d0
Jeffrey Goldsmith <phoenixofstone@yahoo.ca>
2018-03-01 19:25:35 -0500

    Added environment variables and users table

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
-import os
+import os, sys
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app.db.user import db
 
 flask_app = Flask(__name__)
-flask_app.config.from_pyfile('config/settings.cfg')
+
+if os.environ.get('ENVIRONMENT') is None:
+  flask_app.config.from_pyfile('config/settings.cfg')
+
+db.init_app(flask_app)
+migrate = Migrate(flask_app, db)
 
 import app.views, app.endpoints
 
-port = int(os.environ.get("PORT", 5000))
-flask_app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  flask_app.run(host='0.0.0.0', port=port, debug=True)
