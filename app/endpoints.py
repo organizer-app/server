1aaa4e20a7691d98dcd3555a5fd0570a20ee2684
VictorHYu <victor.yu@shopify.com>
2018-03-01 19:43:23 -0500

    resolving merge conflicts

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,9 +1,6 @@
 from app import flask_app
+from app.api.controllers.search_controller import get_recommendations
 
-import sys
-sys.path.insert(0, './app/api')
-import api
-
-@flask_app.route('/test')
-def test():
-    return api.reply('Hello World')
+@flask_app.route('/search')
+def search():
+    return get_recommendations()
