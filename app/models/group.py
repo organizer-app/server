9ab8e38d2ee40c173a9424554c386ff57a8a7649
Justin Oue <justinoue621@gmail.com>
2018-03-02 13:44:00 -0500

    user and group model relationship shit

---
+++ app/models/group.py
@@ -0,0 +1,15 @@
+from app.db.database import database as db
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
+class Group(db.Model):
+  __tablename__ = 'Groups'
+  id = db.Column(db.Integer, primary_key=True)
+  group_name = db.Column(db.String(128))
+  group_type = db.Column(db.String(128))
+  created_at = db.Column(db.DateTime, default=datetime.utcnow())
+  updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+  users = relationship("User", secondary="UsersToGroups")
+
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
