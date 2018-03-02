62917571cc678089713d2e34eb0d0201d5f651a9
Jeffrey Goldsmith <phoenixofstone@yahoo.ca>
2018-03-02 13:26:33 -0500

    Set environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,11 @@ import app.models.user, app.models.place
 
 flask_app = Flask(__name__)
 
-if os.environ.get('ENVIRONMENT') == 'development':
+if os.environ.get('DEVELOPMENT_ENVIRONMENT') != 'production':
   flask_app.config.from_pyfile('config/settings.cfg')
+else:
+  flask_app['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+  flask_app['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 database.init_app(flask_app)
 migrate = Migrate(flask_app, database)
